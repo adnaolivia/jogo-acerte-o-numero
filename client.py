f2d2bc954445083d7cef8bafd03f16151d38fc4c
adna silva <adnaoliveirasilva09@gmail.com>
2024-07-21 20:26:53 -0300

    joguinho Adivinhe o Número

--- client.py
+++ client.py
@@ -2,12 +2,21 @@ import Pyro4
 
 def main():
 
-
+    # ip_address = "ip_aqui"
     uri = input('Insira a URI do servidor: ')
-    calculator = Pyro4.Proxy(uri)  
+    jogo = Pyro4.Proxy(uri)
 
-    print("3 + 5 =", calculator.add(3, 5))
-    print("10 - 7 =", calculator.subtract(10, 7))
+    print("☆*: .｡. Bem vindo ao jogo! Será que você consegue acertar o número sorteado? .｡.:*☆")
+    print("(●'◡'●)")
+    while True:
+        try:
+            sugestao = int(input("Digite um número entre 1 e 100:\n"))
+            resultado = jogo.sugestao(sugestao)
+            print(resultado)
+            if "Parabéns" in resultado:
+                break
+        except ValueError:
+            print("Digite um número válido ¯\_(ツ)_/¯")
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 import Pyro4
+import random
 
 @Pyro4.expose
-class Calculator(object):
-    def add(self, x, y):
-        return x + y
-
-    def subtract(self, x, y):
-        return x - y
+class AdivinheoNumero(object):
+    def __init__(self):
+        self.novo_jogo()
+    
+    def novo_jogo(self):
+        self.numero_sorteado = random.randint(1, 10) # sorteia aleatoriamente um inteiro entre 1 e 100
+        self.tentativas = 0 # inicia em 0, depois vai iterando ate acertar
+    
+    def sugestao(self, numero):
+        self.tentativas += 1 # soma uma nova tentativa a cada erro
+        if numero < self.numero_sorteado:
+            return "Tente um número MAIS ALTO!¯\_(ツ)_/¯\n"
+        elif numero > self.numero_sorteado:
+            return "Tente um número MAIS BAIXO!¯\_(ツ)_/¯\n"
+        else:
+            resultado = f"Parabéns, depois de {self.tentativas} tentativas!╰(*°▽°*)╯"
+            self.novo_jogo()
+            return resultado
 
 # Inicializando o daemon Pyro4
 def main():
     # ip_address = "ip_aqui"
     # port = 00000
     daemon = Pyro4.Daemon()  # Cria um daemon Pyro
-    uri = daemon.register(Calculator)  # Registra o objeto remoto
+    uri = daemon.register(AdivinheoNumero)  # Registra o objeto remoto
     print("Servidor está pronto.")
     print(uri)
     daemon.requestLoop()  # Mantém o servidor rodando
